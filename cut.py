892cbd509530597f0e4ce7fc2dd7cada1ab3c17e
Rohan Shanbhag <rohanshanbhag2001@gmail.com>
2024-03-28 19:01:46 -0500

    Working on ensuring correct Filename provided, or diverting to standard input

--- cut.py
+++ cut.py
@@ -1,9 +1,9 @@
 import sys
-import argparse
 
 printDebugs = True
 
 def filterArgs():
+    if(printDebugs): print("[filterArgs]")
     fields = []
     delimiter = "\t"
     fileName = sys.argv[-1]
@@ -13,20 +13,49 @@ def filterArgs():
             fields = getFieldsToCut(arg)
         if('-d' in arg):
             delimiter = arg.split("-d")[1]
+    # if fileName == fields or fileName == delimiter:
+    #     fileName = ''
     return [fields,delimiter,fileName]
+
+def getFieldsFromFile(fileName,fields,delimiter):
+    if(printDebugs): print("[getFieldsFromFile]")
+    
+    res = ""
+
+    with open(fileName) as file:
+        for lines in file:
+            line = lines.split(delimiter)
+            for field in fields:
+                res += line[int(field) - 1] + delimiter
+            res += "\n"
+    
+    print(res)
+    return
+
+def getFieldsFromStdIn(fileName,fields,delimiter):
+    if(printDebugs): print("[getFieldsFromStdIn]")
 
 def main():
     fields,delimiter,fileName = filterArgs()
+    fileNameProvided = True
+    
     if fields == [] or len(fields) == 1 and fields[0] == '':
         print("[ERROR] Invalid Arguments: Incorrect or missing fields argument")
         return
-    if fileName == sys.argv[0] or '.' not in fileName:
-        print("[ERROR] Invalid Arguments: Incorrect Filename provided")
-        return
-    for field in fields:
-        print("Listing fields: " + str(field))
-    print("Delimiter = " + delimiter)
-    print("FileName = " + fileName)
+    # if fileName == delimiter or fileName == fields or fileName == '' or fileName == '-':
+    #     fileNameProvided = False
+    #     print("fileName = empty")
+    #     fileName = sys.stdin
+    # elif fileName == sys.argv[0] or '.' not in fileName:
+    #     print("[ERROR] Invalid Arguments: Incorrect Filename provided")
+    #     return
+    
+    # for field in fields:
+    #     print("Listing fields: " + str(field))
+    # print("Delimiter = " + delimiter)
+    # print("FileName = " + fileName)
+    if(fileNameProvided): getFieldsFromFile(fileName,fields,delimiter)
+    else: getFieldsFromStdIn(fileName,fields,delimiter)
     
             
 def getFieldsToCut(x):
